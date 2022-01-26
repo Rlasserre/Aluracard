3c1e25a4f594ba4bfdd41eb050edab586d95c0c3
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-01-26 20:28:57 -0300

    add config.json

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function GlobalStyle() {
+    return (
+
+    );
+}
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to Next.Js</h1>
+        </div>
+    );
+}
+export default HomePage;
